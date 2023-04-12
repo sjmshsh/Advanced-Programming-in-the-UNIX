d28cff605996592eb8aaa7b0a41d71a505b8f424
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-04-12 15:44:10 +0800

    Create tcp_server.cc

---
+++ TCP/线程池版/tcp_server.cc
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string>
+#include <string.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "threadpool.h"
+#include "task.h"
+
+#define BACKLOG 5
+
+using namespace std;
+
+class Param
+{
+public:
+    Param(int sock, string ip, int port)
+        : _sock(sock), _ip(ip), _port(port)
+    {
+    }
+
+    int _sock;
+    string _ip;
+    int _port;
+};
+
+class TcpServer
+{
+public:
+    static void *HandlerRequest(void *arg)
+    {
+        // 分离线程
+        pthread_detach(pthread_self());
+
+        Param *p = (Param *)arg;
+        // 线程为客户端提供服务
+        Service(p->_sock, p->_ip, p->_port);
+        delete p; // 释放参数占用的堆空间
+        return nullptr;
+    }
+
+    static void Service(int sock, string client_ip, int client_port)
+    {
+        char buffer[1024];
+        while (true)
+        {
+            ssize_t size = read(sock, buffer, sizeof(buffer) - 1);
+            if (size > 0)
+            {
+                // 读取成功
+                buffer[size] = '\0'; // 打上一个'\0'原因是表示字符串的结尾
+                cout << "get a new link->" << sock << " [" << client_ip << "]:" << client_port << endl;
+
+                write(sock, buffer, size);
+            }
+            else if (size == 0)
+            {
+                // 对端关闭连接
+                cout << client_ip << ":" << client_port << " close!" << endl;
+                break;
+            }
+            else
+            {
+                // 读取失败
+                cerr << sock << " read error!" << endl;
+                break;
+            }
+            close(sock); // 归还文件描述符
+            cout << client_ip << ":" << client_port << " service done!" << endl;
+        }
+    }
+
+    void InitServer()
+    {
+        // 创建套接字
+        _listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (_listen_sock < 0)
+        {
+            cerr << "socket error" << endl;
+            exit(2);
+        }
+        // 绑定
+        struct sockaddr_in local;
+        memset(&local, '\0', sizeof(local));
+        local.sin_family = AF_INET;
+        local.sin_port = htons(_port);
+        local.sin_addr.s_addr = INADDR_ANY;
+
+        if (bind(_listen_sock, (struct sockaddr *)&local, sizeof(local)) < 0)
+        {
+            cerr << "bind error" << endl;
+            exit(3);
+        }
+
+        // 监听
+        if (listen(_listen_sock, BACKLOG) < 0)
+        {
+            cerr << "listen error" << endl;
+            exit(4);
+        }
+    }
+
+    void Start()
+    {
+        _tp->ThreadPoolInit(); // 初始化线程池
+        for (;;)
+        {
+            // 获取连接
+            struct sockaddr_in peer;
+            memset(&peer, '\0', sizeof(peer));
+            socklen_t len = sizeof(peer);
+            int sock = accept(_listen_sock, (struct sockaddr *)&peer, &len);
+            if (sock < 0)
+            {
+                cerr << "accept error, continue next" << endl;
+                continue;
+            }
+            string client_ip = inet_ntoa(peer.sin_addr);
+            int client_port = ntohs(peer.sin_port);
+            cout << "get a new link->" << sock << " [" << client_ip << "]:" << client_port << endl;
+
+            Task task(sock, client_ip, client_port); // 构造任务
+            _tp->Push(task);
+        }
+    }
+
+    TcpServer(int port)
+        : _listen_sock(-1), _port(port)
+    {
+    }
+
+    ~TcpServer()
+    {
+        if (_listen_sock >= 0)
+        {
+            close(_listen_sock);
+        }
+    }
+
+private:
+    int _listen_sock; // 监听套接字
+    int _port;
+    ThreadPool<Task>* _tp; // 线程池
+};
+
+void Usage(string proc)
+{
+    cout << "Usage: " << proc << " port" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        Usage(argv[0]);
+        exit(1);
+    }
+    int port = atoi(argv[1]);
+    TcpServer *svr = new TcpServer(port);
+    svr->InitServer();
+    svr->Start();
+    return 0;
+}
